95fff2b8d0be9d15b7594d498e46fe0a33eb9ab2
kevin <jc.lee@okestro.com>
2026-03-23 16:45:43 +0900

    추천도서 섹션 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,35 @@
 import { ReactNode } from 'react';
 import SearchLayout from '@/components/layout/search-layout';
 import BookItem from '@/components/book-item';
-import { fetchBooks } from '@/lib/book-api';
+import { fetchBooks, fetchRecoBooks } from '@/lib/book-api';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
    const books = await fetchBooks();
+   const recoBooks = await fetchRecoBooks();
    return {
       props: {
          books,
+         recoBooks,
       },
    };
 };
 
-export default function Home({ books }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({ books, recoBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
    return (
-      <div className="flex flex-col gap-3 px-4">
-         {books?.map((book: BookType, index: number) => (
-            <BookItem key={book.id} book={book} />
-         ))}
+      <div className="flex flex-col space-y-5 px-4">
+         <section className="flex flex-col gap-3">
+            <h3 className="text-xl font-bold">추천 도서</h3>
+            {recoBooks?.map((book: BookType) => (
+               <BookItem key={book.id} book={book} />
+            ))}
+         </section>
+         <section className="flex flex-col gap-3">
+            <h3 className="text-xl font-bold">전체 도서</h3>
+            {books?.map((book: BookType, index: number) => (
+               <BookItem key={book.id} book={book} />
+            ))}
+         </section>
       </div>
    );
 }
